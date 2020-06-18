376694618cfafc3869ee5754c40f0126f5196b48
Vladimir Nagaev <nagaevvd1289@gmail.com>
2020-06-18 14:04:36 +0400

    table formatter shuld be working

--- src/main/java/TableFormatter.java
+++ src/main/java/TableFormatter.java
@@ -49,8 +49,66 @@ public class TableFormatter {
         return headerString.toString();
     }
 
-    public String rowWriter(List<ColumnConfig> columnConfigs, List<String> thisRow) {
+    public int howManyRowDefiner(String string) {
+        StringBuilder stringBuilder = new StringBuilder(string);
+        int rows = 1;
+        for (int i = 0; i < stringBuilder.length(); i++) {
+            if (stringBuilder.charAt(i) == '*') {
+                rows++;
+            }
+        }
+        return rows;
+    }
+
+    public String rowWriter(List<ColumnConfig> columnConfigs, List<String> thisRow, int rowCounter) {
         StringBuilder thisString = new StringBuilder();
+        StringTokenizer st = new StringTokenizer(thisRow.get(3), "*", false);
+        List<String> nameData = new ArrayList<String>();
+        while (st.hasMoreTokens()) {
+            nameData.add(st.nextToken());
+        }
+        int willBeRows = howManyRowDefiner(thisRow.get(3));
+
+        if (rowCounter < config.getHeight() - willBeRows) {
+            rowCounter += willBeRows;
+
+        } else {
+            thisString.append(stringDelimiter());
+            thisString.append("\r\n");
+            thisString.append("`");
+            thisString.append("\r\n");
+            rowCounter = 0;
+        }
+
+        for (int i = 0; i < howManyRowDefiner(thisRow.get(3)); i++) {
+            for (int j = 0; j < columnConfigs.size(); j++) {
+                if (j != 3) {
+                    int counter = 0;
+                    thisString.append("|");
+                    thisString.append(" ");
+                    counter += 2;
+                    thisString.append(thisRow.get(j));
+                    counter += thisRow.get(j).length();
+                    while (counter < columnConfigs.get(j).getWidth() - 1) {
+                        thisString.append(" ");
+                        counter++;
+                    }
+                    thisString.append("|");
+
+                } else {
+                    int counter = 0;
+                    thisString.append("|");
+                    thisString.append(" ");
+                    counter += 2;
+                    thisString.append(nameData.get(i));
+
+
+                }
+
+            }
+            thisString.append("\r\n");
+
+        }
 
 
         return thisString.toString();
@@ -60,9 +118,9 @@ public class TableFormatter {
         this.defaultDelimiter = defaultDelimiter;
     }
 
-    public int stringDivider(String string, int width) { // divides names for parts to write in into stream
+    public String stringDivider(String string, int width) { // divides names for parts to write in into stream
         // this void will chsnge inputed string to " Иван * ов Ки * рилл" where * is row delimiter
-        int stringCounter = 1;
+        StringBuilder outputString = new StringBuilder();
         List<String> currentName = new ArrayList<String>();
         StringTokenizer currentNameTokenazer = new StringTokenizer(string, " ", false);
         while (currentNameTokenazer.hasMoreTokens()) {
@@ -82,30 +140,52 @@ public class TableFormatter {
         }
 
 
-        return stringCounter;
+        return outputString.toString();
     }
 
     public void format(InputStream inputStream, OutputStream outputStream) throws IOException {
+        int rowCounter = 0;
         String currentLine;
         StringBuilder builder = new StringBuilder();
 
         BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
         currentLine = reader.readLine();
+        builder.append(headRawWriter(config.getColumnConfigs()));// шапка
+        builder.append("\r\n");
+        rowCounter++;
+        builder.append(stringDelimiter()); // разделитель
+        builder.append("\r\n");
+        rowCounter++;
+
         while (currentLine != null) {
             StringTokenizer currentStringTokenazer = new StringTokenizer(currentLine, defaultDelimiter, false);
             ArrayList<String> list = new ArrayList<String>();
+
             while (currentStringTokenazer.hasMoreTokens()) {
                 list.add(currentStringTokenazer.nextToken());
             }
 
             // TODO createStrings
-            builder.append(headRawWriter(config.getColumnConfigs()));
-            builder.append("\r\n");
+
+            builder.append(rowWriter(config.getColumnConfigs(), list, rowCounter)); // сама строка
             builder.append(stringDelimiter());
             builder.append("\r\n");
-            builder.append(rowWriter(config.getColumnConfigs(), list));
-            builder.append(stringDelimiter());
-            builder.append("\r\n");
+            rowCounter++;
+            if (rowCounter >= config.getHeight() - 2) {
+                builder.append("\r\n");
+                builder.append("`");
+                builder.append("\r\n");
+                rowCounter = 0;
+                builder.append(headRawWriter(config.getColumnConfigs()));// шапка
+                builder.append("\r\n");
+                rowCounter++;
+                builder.append(stringDelimiter()); // разделитель
+                builder.append("\r\n");
+                rowCounter++;
+
+            } else {
+                continue;
+            }
 
 
         }
